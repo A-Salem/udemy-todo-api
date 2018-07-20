163bcb12aee77b3af14aee4650d8943e3f39fe13
Ahmed Salem <a.salem@arabhosters.com>
2018-07-20 07:21:29 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -78,6 +78,27 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  // Model
+  let User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise(function(resolve, reject) {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   let user = this;
 
